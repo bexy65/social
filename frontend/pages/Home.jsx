91bdf5cbc6ba1c28a3eb5c83c8d721c6e83dbbcc
bexy65 <naim.ertan98@gmail.com>
2026-09-07 14:40:36 +0300

    feat: basic home page with dummy data

---
+++ frontend/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+function Home() {
+    const [count, setCount] = useState(0)
+    return (
+        <>
+            <h1>Test home</h1>
+        </>
+    )
+}
+
+export default Home;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from '../pages/Home'
 
 function App() {
 
   return (
     <>
-
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
     </>
   )
 }
