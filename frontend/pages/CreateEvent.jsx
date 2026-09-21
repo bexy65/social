4620ccb1e66952418459497d272f815bd7d6257e
bexy65 <naim.ertan98@gmail.com>
2026-09-21 14:51:00 +0300

    feat: #1 create event mockup data and layout

--- frontend/pages/CreateEvent.jsx
+++ frontend/pages/CreateEvent.jsx
@@ -1,8 +1,10 @@
+import { CreateRaidEventForm } from "../components/CreateRaidEventForm";
+
 function CreateEvent() {
     return (
-        <>
-            <p>Create event</p>
-        </>
+        <div className="max-w-4xl mx-auto p-6 border my-3">
+            <CreateRaidEventForm />
+        </div>
     )
 }
 
